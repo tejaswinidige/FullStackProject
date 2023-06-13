8d9905b7804241427b815ea48c24843197a8e184
Admin <Admin@DESKTOP-O2C96HO>
2023-06-13 10:25:29 +0530

    SpringBoot Files

---
+++ SpringAngular/src/main/java/com/Project/SpringAngular/CustomerRepo/CustomerRepo.java
@@ -0,0 +1,15 @@
+package com.Project.SpringAngular.CustomerRepo;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.stereotype.Repository;
+
+import com.Project.SpringAngular.DTO.CustomerDTO;
+import com.Project.SpringAngular.Entity.Customer;
+
+
+@EnableJpaRepositories
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer,Integer> {
+}
